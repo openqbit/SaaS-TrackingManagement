a39746237884328ea4c28d6d54e33da5f6836642
Amandi Gamage <Amandiimasha43@gmail.com>
2017-05-23 16:54:32 +0530

    Created Root & RootDetail Models

---
+++ OpenQbit.TrakingManagementSystem.git/OpenQbit.TrakingManagement.Common.Models/RootDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.TrakingManagement.Common.Models
+{
+    public class RootDetail
+    {
+        public int RootDetailID { get; set; }
+
+        public string City { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string RootID { get; set; }
+
+        public virtual Root Root { get; set; }
+    }
+}
