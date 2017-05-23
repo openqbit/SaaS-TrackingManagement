d83ebb62410b5f4fb4c9604cf50499ffac7c8322
K M Uditha Sampath <udithasampathkm95@gmail.com>
2017-05-23 16:17:07 +0530

    Create Tour  & Vehicle Models

---
+++ OpenQbit.TrakingManagementSystem.git/OpenQbit.TrakingManagement.Common.Models/Tour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.TrakingManagement.Common.Models
+{
+   public class Tour
+    {
+        public int TourID { get; set; }
+
+        public string TourType { get; set; }
+
+        public string Location { get; set; }
+
+        public string Root { get; set; }
+
+        public int Speed { get; set; }
+
+        public virtual ICollection<Vehicle> Vehicle { get; set; }
+
+        public virtual ICollection<Driver> Driver { get; set; }
+    }
+}
