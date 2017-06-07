77379aa5cbd234735ce0be4928448998ceb8c091
Jithmal Asela <jithmal95@gmail.com>
2017-06-07 13:21:43 +0530

    DAL Added

---
+++ OpenQbit.TrakingManagementSystem.git/OpenQbit.TrakingManagement.DAL/DBContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using OpenQbit.TrakingManagement.Common.Models;
+
+namespace OpenQbit.TrakingManagement.DAL
+{
+    public class DBContext : DbContext
+
+    {
+        public DBContext() : base("TrackinDB")
+        {
+        }
+        public DbSet<Driver> Driver { get; set; }
+        public DbSet<Resource> Resource { get; set; }
+        public DbSet<Root> Root { get; set; }
+        public DbSet<RootDetail> RootDetail { get; set; }
+        public DbSet<Tour> Tour { get; set; }
+        public DbSet<Vehicle> Vehicle { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ OpenQbit.TrakingManagementSystem.git/OpenQbit.TrakingManagement.DAL/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace OpenQbit.TrakingManagement.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OpenQbit.TrakingManagement.DAL.DBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OpenQbit.TrakingManagement.DAL.DBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
