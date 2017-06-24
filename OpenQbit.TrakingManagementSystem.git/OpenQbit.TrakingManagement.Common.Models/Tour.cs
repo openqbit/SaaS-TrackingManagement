1b8e7b47230e1c697ae8b0fa61c23d0855064ec5
ABC <ABC@DESKTOP-6135F2D>
2017-06-24 16:26:30 +0530

    update

---
+++ OpenQbit.TrakingManagementSystem.git/OpenQbit.TrakingManagement.Common.Models/Tour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.TrakingManagement.Common.Models
+{
+    public class Tour
+    {
+        public int tourID;
+        public String memberName;
+
+             
+    }
+}
